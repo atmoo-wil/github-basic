85a05b915ac48615534a20f54f0869d51b23f016
atmoo-wil <wilaga.atmo@gmail.com>
2022-10-14 14:24:56 +0700

    membuat branch produk

---
+++ script.js
@@ -0,0 +1,54 @@
+// day 1, tipe data
+    // let number = 12;
+    // let string = 'Belajar bersama';
+    // let object={
+    //     nama: 'wira',
+    //     umur: 21,
+    //     kerja: false,
+    // }
+
+    // let array= [11,21,31,41,51];
+
+// day 2,  condition, operator, prompt
+    let umur = 21;
+
+
+    if (umur<30) {
+        
+    } else {
+        
+    }
+
+// Pengulangan/Loop
+    for (let i = 0; i <= 10; i++) {
+       console.log(i);
+    }
+    let array = ['agis', 'atmo', 21];
+    let object = [{
+        fname: 'wira',
+        lname: 'atmo',
+        umur: 21,
+    }]
+
+    for (key in array) {
+        console.log(key);
+    }
+
+    for (key of   array) {
+        console.log(key);
+    }
+
+    for (key of array) {
+        console.log(key);
+    }
+
+
+// while 
+    let i=0;
+    do {
+        console.log(i);
+        i++
+    } while (i<=10) {
+        
+    }
+
